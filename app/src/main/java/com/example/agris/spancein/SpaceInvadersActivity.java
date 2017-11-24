df3c7e4b2968a4bf506510d75d88435f287a9098
agris <testetajsagris@gmail.com>
2017-11-24 11:19:25 +0200

    Initial commit

---
+++ app/src/main/java/com/example/agris/spancein/SpaceInvadersActivity.java
@@ -0,0 +1,13 @@
+package com.example.agris.spancein;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SpaceInvadersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_space_invaders);
+    }
+}
